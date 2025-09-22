7c16db6fb7402cf7f19caec9c47236b30b512781
Max Carrasco <max.carrasco.h@gmail.com>
2025-09-21 23:50:42 -0500

    apis finish

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,8 +1,8 @@
 import prisma from '@/lib/prisma'
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 export async function GET(request: NextRequest) { 
-
     const searchParams = request.nextUrl.searchParams;
     const take = Number(searchParams.get('take') ?? '10');
     const skip = Number(searchParams.get('skip') ?? '0');
@@ -22,4 +22,19 @@ export async function GET(request: NextRequest) {
     const todos = await prisma.todo.findMany({take,skip});
 
     return NextResponse.json(todos);
+}
+
+const postSchema = yup.object({
+    description: yup.string().required(),
+    complete: yup.boolean().optional().default(false),
+})
+
+export async function POST(request: Request) { 
+    try {
+        const { description, complete } = await postSchema.validate(await request.json());
+        const todo = await prisma.todo.create({ data: { description, complete}});
+        return NextResponse.json(todo);
+    } catch (error) {
+        return NextResponse.json(error, { status: 400 });
+    }
 }
